abe0a557395a9bf6748e4ea142461994107a4d9d
coomsn <61256281+coomsn@users.noreply.github.com>
2024-05-15 23:16:27 +0800

    Update start.sh

--- mihomo/scripts/start.sh
+++ mihomo/scripts/start.sh
@@ -8,11 +8,13 @@ red="\033[91m"
 cd ${0%/*}
 source ./mihomo.scripts
 
+inot_gid=20003
+
 if [ ! -f ${MODDIR}/disable ]; then
 	run_proxy
 fi
 if pgrep inotifyd > /dev/null 2>&1 ; then
-  pkill -g 20000
+  pkill -g ${inot_gid}
 fi
-${busybox} setuidgid 0:20000 inotifyd "${SCRIPTS_DIR}/mihomo.inotify" "${MODDIR}" > /dev/null 2>&1 &
+${busybox} setuidgid 0:${inot_gid} inotifyd "${SCRIPTS_DIR}/mihomo.inotify" "${MODDIR}" > /dev/null 2>&1 &
 echo -e "${green}now,mihomo.inotify is running with PID ${red}$(pgrep inotifyd)${green}.${normal}"
